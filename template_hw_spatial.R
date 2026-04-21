92d4491a61ad23e23c11075837b14d228b609016
angela-pm <pame.923.mp@gmail.com>
2026-04-20 23:14:07 -0500

    Update template_hw_spatial.R

--- template_hw_spatial.R
+++ template_hw_spatial.R
@@ -1,22 +1,28 @@
 rm(list = ls())
 #setwd("")
 
-gpklink="https://github.com/CIENCIA-DE-DATOS-ESPACIALES1/Actividad-1/raw/main/MapFromPython1.gpkg"
-jsonlink="https://github.com/CIENCIA-DE-DATOS-ESPACIALES1/Actividad-1/raw/main/MapFromPython2.geojson"
+gpklink="https://github.com/CIENCIA-DE-DATOS-ESPACIALES1/Actividad-1/raw/main/China/china_6933.gpkg"
 
 library(sf)
 
-map1=read_sf(gpklink, layer = "usa")
-map2=read_sf(jsonlink)
+map1=read_sf(gpklink, layer = "puntos_ddm")
 
 library(ggplot2)
-map1gg=ggplot() + theme_bw() + geom_sf(data = map1,fill='red', color='grey90')
+map1gg=ggplot() + theme_bw() + geom_sf(data = map1,color='red')
 map1gg
 
-saveRDS(map1gg,file='map1.rds')
+saveRDS(map1gg,file='map1_final.rds')
 
+map2=read_sf(gpklink, layer = "centroides_psm")
 
-
-map2gg=ggplot() + theme_void() + geom_sf(data = map2)
+map2gg=ggplot() + theme_void() + geom_sf(data = map2, aes(size=size))
 map2gg
-saveRDS(map2gg,file='map2.rds')
+
+saveRDS(map2gg,file='map2_final.rds')
+
+map3=read_sf(gpklink, layer = "provincias_densidad")
+
+map3gg=ggplot() + theme_void() + geom_sf(data = map3, aes(fill=densidad_cat_nombre))
+map3gg
+
+saveRDS(map3gg,file='map3_final.rds')
